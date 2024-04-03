8c4d8e10ac31b192b5d05d94afb3305aae7bbbf6
HEPSIBA JALLA <jallahepsiba@gmail.com>
2024-04-03 16:45:11 +0530

    This is a program on Arthimatic

---
+++ arthimatic.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class arthimatic{
+	public static void main(String[] args){
+		int a,b;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter two numbers");
+		a=sc.nextInt();
+		b=sc.nextInt();
+		System.out.println("a="+a+"b="+b);
+		System.out.println("Addition of two numbers="+(a+b));
+		System.out.println("Subtraction of two numbers="+(a-b));
+		System.out.println("Multipilication of two numbers="+(a*b));
+		System.out.println("Division of two numbers="+(a/b));
+		System.out.println("Modulas of two numbers="+(a%b));
+	}
+}
